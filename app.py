9e0bfe68239c49b0c290c4857e82c4c02d914356
Jeoás P. Silva <76924833+jeoaspereirasilva@users.noreply.github.com>
2021-09-06 23:10:40 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+'''
+    06 setembro 2021 | 22:02
+    
+    Iniciando Desenvolvimento Web com
+    Python e o Framework Flask
+'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
